e8cb829936c7c787acaaaeb2a8935785639067cf
naveenprasanth2 <78147993+naveenprasanth2@users.noreply.github.com>
2023-06-25 21:31:40 +0530

    tillVideo102

--- lib/helper/sizebox_helper.dart
+++ lib/helper/sizebox_helper.dart
@@ -4,4 +4,5 @@ class SizedBoxHelper{
   static SizedBox sizeBox12 = const SizedBox(height: 12,);
   static SizedBox sizeBox20 = const SizedBox(height: 20,);
   static SizedBox sizeBox30 = const SizedBox(height: 30,);
+  static SizedBox sizeBox40 = const SizedBox(height: 40,);
 }
